564fefb59158923335f5e39b23d8cb4276f6b2e7
DESKTOP-R1ERUUB\vjava <vjava114@gmail.com>
2019-05-03 00:09:16 +0900

    Sout을 assertThat() 으로 테스트하도록 변경 하였습니다. 추가로 없는이름으로 찾을시 예외발생하는지 테스트도 추가하였습니다.

--- src/test/java/bonjugi/GenreTest.java
+++ src/test/java/bonjugi/GenreTest.java
@@ -3,13 +3,29 @@ package bonjugi;
 import bonjugi.model.Genre;
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class GenreTest {
 
 	@Test
 	public void 한글이름으로_장르찾기() {
 
 		Genre movie = Genre.valueOf("영화");
-		System.out.println(movie);
+		assertThat(movie).isEqualTo(Genre.영화);
+
+		Genre documentary = Genre.valueOf("다큐멘터리");
+		assertThat(documentary).isEqualTo(Genre.다큐멘터리);
+
+		Genre sports = Genre.valueOf("스포츠");
+		assertThat(sports).isEqualTo(Genre.스포츠);
+
 
 	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void 한글이름으로_장르찾기_없는이름으로는_찾을수없음(){
+		Genre.valueOf("도큐멘타링");
+	}
+
+
 }
